e771ec24de26a85f23f94dd37eb67ce6a6e6bf0f
Mohamed Karawia <mohamedkarawia@Mohameds-MacBook-Air.local>
2023-09-05 15:59:26 +0300

    render setup

--- project_management/settings.py
+++ project_management/settings.py
@@ -27,7 +27,9 @@ SECRET_KEY = 'django-insecure-zsfyt%(=kub-!(mpd08pazy&x0(15_x$oxm%gixoe34)ok%*r)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'django-pm.render.com', '127.0.0.1'
+]
 
 
 # Application definition
@@ -46,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -129,6 +132,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
